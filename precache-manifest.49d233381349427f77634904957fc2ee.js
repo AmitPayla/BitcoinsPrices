714ac0c0e051ffb467bc9427b9ed45f90f876557
Amit <aspayla76@gmail.com>
2020-08-01 06:22:35 +0530

    Updates

---
+++ precache-manifest.49d233381349427f77634904957fc2ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc09859a0c7fd38929460fe46aa1382",
+    "url": "/BitcoinsPrices/index.html"
+  },
+  {
+    "revision": "20a216454e1fe2f69454",
+    "url": "/BitcoinsPrices/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6b330ce98c10f355d834",
+    "url": "/BitcoinsPrices/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "20a216454e1fe2f69454",
+    "url": "/BitcoinsPrices/static/js/2.8fe74f2c.chunk.js"
+  },
+  {
+    "revision": "4f44a17bb08e78ed1b92485defc539ac",
+    "url": "/BitcoinsPrices/static/js/2.8fe74f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b330ce98c10f355d834",
+    "url": "/BitcoinsPrices/static/js/main.8a39bdbc.chunk.js"
+  },
+  {
+    "revision": "feaf3ed6a238c976aafb",
+    "url": "/BitcoinsPrices/static/js/runtime-main.a6a203b4.js"
+  }
+]);
